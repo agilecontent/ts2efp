ec057148eef6463c9243845395f06bbc7bf36d2f
Anders Cedronius <anders.cedronius@icloud.com>
2020-02-10 22:03:06 +0100

    added EFP

--- main.cpp
+++ main.cpp
@@ -4,17 +4,27 @@
 #include "mpegts_demuxer.h"
 #include "simple_buffer.h"
 #include "ts_packet.h"
+#include "ElasticFrameProtocol.h"
 
+#define MTU 1456 //SRT-max
 #define LISTEN_INTERFACE "127.0.0.1"
 #define LISTEN_PORT 8080
 
-int main() {
+ElasticFrameProtocol myEFPSend(MTU, ElasticFrameMode::sender);
 
+void sendData(const std::vector<uint8_t> &subPacket) {
+  std::cout << "Boom!" << std::endl;
+}
+
+int main() {
+  std::cout << "TS-2-EFP Listens at: " << LISTEN_INTERFACE << ":" << unsigned(LISTEN_PORT) << std::endl;
 
   std::ofstream out_adts("out.adts", std::ios::binary);
   std::ofstream out_h264("out.h264", std::ios::binary);
 
-  std::cout << "TS-2-EFP Listens at: " << LISTEN_INTERFACE << ":" << unsigned(LISTEN_PORT) << std::endl;
+  myEFPSend.sendCallback = std::bind(&sendData, std::placeholders::_1);
+  ElasticFrameMessages efpMessage;
+
   SimpleBuffer in;
   MpegTsDemuxer demuxer;
   kissnet::udp_socket serverSocket(kissnet::endpoint(LISTEN_INTERFACE, LISTEN_PORT));
@@ -32,10 +42,37 @@ int main() {
       std::cout << "GOT: " << unsigned(frame->stream_type);
       if (frame->stream_type == 0x0f) {
         std::cout << " AAC Frame";
-        out_adts.write(frame->_data->data(), frame->_data->size());
+       // out_adts.write(frame->_data->data(), frame->_data->size());
+        efpMessage = myEFPSend.packAndSendFromPtr((const uint8_t*)frame->_data->data(),
+                                                  frame->_data->size(),
+                                                  ElasticFrameContent::adts,
+                                                  frame->pts,
+                                                  frame->pts,
+                                                  EFP_CODE('A', 'D', 'T', 'S'),
+                                                  21,
+                                                  NO_FLAGS
+        );
+
+        if (efpMessage != ElasticFrameMessages::noError) {
+          std::cout << "h264 packAndSendFromPtr error " << std::endl;
+        }
       } else if (frame->stream_type == 0x1b) {
         std::cout << " H.264 frame";
-        out_h264.write(frame->_data->data(), frame->_data->size());
+       // out_h264.write(frame->_data->data(), frame->_data->size());
+        efpMessage = myEFPSend.packAndSendFromPtr((const uint8_t*)frame->_data->data(),
+            frame->_data->size(),
+            ElasticFrameContent::h264,
+            frame->pts,
+            frame->dts,
+            EFP_CODE('A', 'N', 'X', 'B'),
+            20,
+            NO_FLAGS
+            );
+
+         if (efpMessage != ElasticFrameMessages::noError) {
+           std::cout << "h264 packAndSendFromPtr error " << std::endl;
+         }
+
       }
 
       std::cout << " completed: " << unsigned(frame->_data->size()) << " " << unsigned(frame->pts) ;
@@ -45,5 +82,5 @@ int main() {
   }
   out_h264.close();
   out_adts.close();
-  return 0;
+  return EXIT_SUCCESS;
 }
